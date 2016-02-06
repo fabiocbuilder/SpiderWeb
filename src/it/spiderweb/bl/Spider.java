db3ce69ef047167981e5259a1737f039057381b3
Fabio <Fabio@FABIO-PC>
2016-02-06 12:39:33 +0100

    ricerca con delay anti-prevent bot

--- src/it/spiderweb/bl/Spider.java
+++ src/it/spiderweb/bl/Spider.java
@@ -17,6 +17,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Constructs a spider/crawler for web pages. A spider web can search and found
@@ -239,25 +241,31 @@ public class Spider {
      */
     public String getJsonArray() throws FileNotFoundException, IOException, HTMLParseException, ScriptException {
         HTMLDocument htmlDocument = HTMLDocumentFactory.buildDocument(url);
-        String JSON = "";
+        File script_pagine = new File("src/it/spiderweb/getpage-blankpages.js");
+        String doc_url = url.toString();
+        
+        String JSON = "{\"users-info\":[";
 
         int current_page = 1;
-        int tot_page = (int) WebGrabber.grab(htmlDocument, "src/it/spiderweb/getpage-blankpages.js");
-
-        do {
-            url = new URL(url.toString() + "&p=" + current_page++);
-            HTMLDocument htmlDocumentToScan = HTMLDocumentFactory.buildDocument(url);
-            JSON += getJsonArray(htmlDocumentToScan, criteria);
-        } while (current_page <= tot_page);
-
+        int tot_page = Integer.valueOf(""+WebGrabber.grab(htmlDocument, script_pagine));
+        try {
+            do {
+                url = new URL(doc_url + "&p=" + current_page++);
+                HTMLDocument htmlDocumentToScan = HTMLDocumentFactory.buildDocument(url);
+                JSON += getJsonArray(htmlDocumentToScan, criteria);
+                Thread.sleep(10000);              
+            } while (current_page <= tot_page);
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        JSON = JSON.substring(0,JSON.length()-1) + "]}"; //cava l'ultima virgola (di eccesso, posta dopo l'ultima parentesi graffa, prima di chiudere il json
         return JSON;
     }
 
     //--------------------------------------------------------------------------
     /**
-     * Get a JSON array from a given <b>htmlDocument</b> with a
-     * <b>blLogicFile</b>
-     *
+     * Get a JSON array from a given <b>htmlDocument</b> with a <b>blLogicFile</b>
      * @param htmlDocument
      * @param criteria
      * @return
